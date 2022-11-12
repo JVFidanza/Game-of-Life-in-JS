efbca3824c74d0539f38aee1423ef625bedbf612
João Vítor Fidanza Artidão <vitor.fidanza@gmail.com>
2022-11-11 22:24:51 -0300

    feat: building cell grids with cell states as css class

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 let masterBody = document.getElementById('masterBody');
 let gridSize = 25;
 
+//Initiate cell matrix
+let rows = new Array(gridSize);
+rows.fill(false);
+let gridMatrix = rows.map(x => [...rows]);
+gridMatrix[3][9] = true;
+//
+console.log(gridMatrix);
 
-//Initiate grid
-let cell = document.createElement('div');
-cell.className = 'cell';
-for (let iExt = 0; iExt < gridSize; iExt++) {
-    let cellLine = document.createElement('div');
-    cellLine.className = 'cellLine';
-    for (let iInt = 0; iInt < gridSize; iInt++) {
-        let cellUnit = cell.cloneNode();
-        cellUnit.id = `cell[${iExt + 1}][${iInt + 1}]`;
-        cellLine.append(cellUnit);        
+//Initiate visual grid
+renderGrid(gridMatrix);
+function renderGrid(cellMatrix){
+    let cell = document.createElement('div');
+    cell.className = 'cell';
+    masterBody.querySelectorAll('*').forEach(x => x.remove());
+    cellMatrix.forEach((cellRow, iExt) => {
+        try{
+        let cellLine = document.createElement('div');
+        cellLine.className = 'cellLine';
+        cellRow.forEach((cellData, iInt) => {
+            let cellUnit = cell.cloneNode();
+            cellUnit.id = `cell[${iExt + 1}][${iInt + 1}]`;
+            cellUnit.classList.add(cellData? 'aliveCell' : 'deadCell');
+            cellLine.append(cellUnit);   
+        });
+        masterBody.append(cellLine);
     }
-    masterBody.append(cellLine);
+    catch{}
+    });
 }
+//
